49d86c000d302779913a91ea8e5f40736d612d2e
larissa <larissamarques.ufam@gmail.com>
2023-03-10 14:39:41 -0300

    Criação do repositório

--- M1S04/Repositorio.cs
+++ M1S04/Repositorio.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace M1S04
 {
-    public class Repositorio
+    public static class Repositorio
     {
-    }
+        private static List<Bebida> ListaBebida { get; set; }
+        private static List<Suco> ListaSuco { get; set; }
+        private static List<Refrigerante> ListaRefrigerante { get; set; }
+     }
 }
